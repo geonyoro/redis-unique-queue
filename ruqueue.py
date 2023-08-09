b2c8ca5bfca8f17e6debfe05bf047aaca58eb79a
George Nyoro <geonyoro@gmail.com>
2023-08-09 15:30:48 +0300

    feat: Refactor modules and methods access

--- ruqueue.py
+++ ruqueue.py
@@ -15,9 +15,13 @@ def generate_random_string(length):
     )
 
 
-class RedisUniqueQueue:
+class Queue:
     """
-    Redis extended producer queue that ensures every item pushed onto it is unique and expires if processing fails.
+    Redis extended producer queue that ensures every item pushed onto it is unique.
+
+    The interface is modelled to have `some` of the same methods as queue.Queue.
+    When an item is fetched from the queue, a bound is set on how long processing will
+    take.
     """
 
     def __init__(
@@ -43,19 +47,41 @@ class RedisUniqueQueue:
             "list": f"{var_prefix}:list",
             "zset": f"{var_prefix}:expiry:zset",
         }
+        LOGGER.debug("Vars created are %s", self._vars)
 
-    def add(self, item, key: str) -> bool:
+    def _sismember(self, key):
+        return self._redis_conn.sismember(self._vars["set"], key)
+
+    def _sadd(self, key):
+        return self._redis_conn.sadd(self._vars["set"], key)
+
+    def _srem(self, key):
+        return self._redis_conn.srem(self._vars["set"], key)
+
+    def _zrangebyscore(self, *args):
+        return self._redis_conn.zrangebyscore(self._vars["zset"], *args)
+
+    def _zrem(self, key):
+        return self._redis_conn.zrem(self._vars["zset"], key)
+
+    def _zadd(self, mapping):
+        return self._redis_conn.zadd(self._vars["zset"], mapping)
+
+    def _lpop(self):
+        return self._redis_conn.lpop(self._vars["list"])
+
+    def put(self, item, key: str) -> bool:
         """
         push an item to the internal list, if it doesn't exist in the set.
         """
         self.clear_expired()
-        set_name = self._vars["set"]
-        if self._redis_conn.sismember(set_name, key):
-            LOGGER.debug("Key %s already existed in the set %s.", key, set_name)
+
+        if self._sismember(key):
+            LOGGER.debug("Key %s already existed in the set.", key)
             return False
 
         # TODO: wrap this as a transaction
-        self._redis_conn.sadd(set_name, key)
+        self._sadd(key)
         pickled_item = pickle.dumps({"key": key, "item": item})
         self._redis_conn.rpush(self._vars["list"], pickled_item)
 
@@ -63,25 +89,18 @@ class RedisUniqueQueue:
 
     def clear_expired(self):
         removed = []
-        for key in self._redis_conn.zrangebyscore(
-            self._vars["zset"], 0, int(time.time())
-        ):
-            # remove from the set
-            self._redis_conn.srem(self._vars["set"], key)
-            # remove from the zset
-            self._redis_conn.zrem(self._vars["zset"], key)
+        for key in self._zrangebyscore(0, int(time.time())):
+            self._srem(key)
+            self._zrem(key)
             removed.append(key)
         return removed
 
     def get(self):
-        pickled_item = self._redis_conn.lpop(self._vars["list"])
+        pickled_item = self._lpop()
         unpickled_item = pickle.loads(pickled_item)
 
         key = unpickled_item["key"]
         item = unpickled_item["item"]
         expiry_time = int(time.time() + self._expiry_in_secs)
-        self._redis_conn.zadd(
-            self._vars["zset"],
-            {key: expiry_time},
-        )
+        self._zadd({key: expiry_time})
         return item
